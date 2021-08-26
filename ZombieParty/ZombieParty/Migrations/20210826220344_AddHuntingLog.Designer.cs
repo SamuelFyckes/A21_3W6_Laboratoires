33285bae9eb6157cd538aca590303f3540e07104
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-08-26 18:04:59 -0400

    FCT HuntingLog
    Création de la table HuntingLog

--- ZombieParty/ZombieParty/Migrations/20210826220344_AddHuntingLog.Designer.cs
+++ ZombieParty/ZombieParty/Migrations/20210826220344_AddHuntingLog.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZombieParty.Models.Data;
 
 namespace ZombieParty.Migrations
 {
     [DbContext(typeof(ZombiePartyDbContext))]
-    partial class ZombiePartyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210826220344_AddHuntingLog")]
+    partial class AddHuntingLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
