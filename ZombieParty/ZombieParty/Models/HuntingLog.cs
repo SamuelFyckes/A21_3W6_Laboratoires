30cc1977eaae89e651dd5a11b7c1c4a273a01cf3
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-09-07 01:56:24 -0400

    FCT ZombieHuntingLog navigation
    AJout de la propriété de navigation

--- ZombieParty/ZombieParty/Models/HuntingLog.cs
+++ ZombieParty/ZombieParty/Models/HuntingLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,5 +11,11 @@ namespace ZombieParty.Models
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+
+    // Propriété de navigation vers zombieHuntingLog
+    //OBLIGATOIRE Pour la relation 1 à plusieurs avec zombieHuntingLog
+    public ICollection<ZombieHuntingLog> zombieHuntingLogs { get; set; }
+
+   
   }
 }
