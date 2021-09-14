4d62f0b6e01dd3d5786de8e82a8d6229cefb5004
ValerieTurgeon <valerie.turgeon.cem@gmail.com>
2021-09-14 07:44:55 -0400

    FCT Tests Unitaires NUnit

---
+++ Appl_TestsUnitaires/App_TU_TestsN/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace App_TU_TestsN
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}
