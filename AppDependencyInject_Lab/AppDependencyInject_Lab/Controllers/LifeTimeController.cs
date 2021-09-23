70151897c172a24c2d4d6c80cef7e6b3ef7a10d1
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-09-22 23:59:39 -0400

    INIT Creation initiale du projet

---
+++ AppDependencyInject_Lab/AppDependencyInject_Lab/Controllers/LifeTimeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace AppDependencyInject_Lab.Controllers
+{
+  public class LifeTimeController : Controller
+  {
+    #region Créer une propriété pour chaque service lifeTime ici
+   
+
+    #endregion
+
+    #region Modifier le constructor controller ici
+    public LifeTimeController()
+    {
+
+    }
+    #endregion
+
+
+    public IActionResult Index()
+    {
+      #region Afficher les ID des trois lifeTime ici
+      
+      #endregion
+
+      return View();
+    }
+  }
+}
